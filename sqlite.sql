9f17739200902755a9798acd0f9131e235d2b624
Kara Bayse <karabayse@gmail.com>
2019-02-27 17:52:21 -0600

    non-correlated subquery

--- sqlite.sql
+++ sqlite.sql
@@ -1,5 +1,20 @@
 -- LIMIT
--- Select 10 rows 
+-- Select 10 rows
 SELECT *
 FROM flights
 LIMIT 10;
+
+
+-- Non-Correlated Subqueries
+-- First create an inner query, or subquery
+SELECT code
+FROM airports
+WHERE elevation > 2000;
+
+-- Then take the result of the inner query and use it to filter
+SELECT *
+FROM flights
+WHERE origin in (
+    SELECT code 
+    FROM airports
+    WHERE elevation > 2000);
